f00d156cdfc69f025295831ac76759934917ec74
Ossas <157389555+nt-ossas@users.noreply.github.com>
2025-03-04 17:59:20 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,142 +1,113 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const container = document.querySelector('.container');
+function addBoxEventListener(box) {
+    box.addEventListener('click', () => {
+        const container = document.querySelector('.container');
+        if (box.classList.contains('active')) {
+            box.classList.remove('active');
+            setTimeout(() => {
+                container.prepend(box);
+            }, 400);
+        } else {
+            box.classList.add('active');
+            setTimeout(() => {
+                container.appendChild(box);
+            }, 400);
+        }
+    });
+}
 
-    function addBoxEventListener(box) {
-        box.addEventListener('click', () => {
-            if (box.classList.contains('active')) {
-                box.classList.remove('active');
-                setTimeout(() => {
-                    container.prepend(box);
-                }, 400);
-            } else {
-                box.classList.add('active');
-                setTimeout(() => {
-                    container.appendChild(box);
-                }, 400);
-            }
-        });
-    }
+function addTitleEventListener(title) {
+    title.addEventListener('click', (event) => {
+        event.stopPropagation();
 
-    function addTitleEventListener(title) {
-        title.addEventListener('click', (event) => {
-            event.stopPropagation();
-
-            const allTitles = document.querySelectorAll(".title h6");
-            allTitles.forEach(element => {
-                if (element !== title) {
-                    element.classList.remove("selected");
-                    const icon = element.querySelector('i');
-                    if (icon) {
-                        icon.remove();
-                    }
-                }
-            });
-
-            const isSelected = title.classList.contains("selected");
-
-            if (isSelected) {
-                title.classList.remove("selected");
-                const icon = title.querySelector('i');
+        const allTitles = document.querySelectorAll(".title h6");
+        allTitles.forEach(element => {
+            if (element !== title) {
+                element.classList.remove("selected");
+                const icon = element.querySelector('i');
                 if (icon) {
                     icon.remove();
                 }
-                const selection = document.getElementById("select-container");
-                selection.innerHTML = `<div class="box">
-                                            <img src="">
-                                            <div class="title">
-                                                <h6>Seleziona il tuo album cliccando sul nome</h6>
-                                            </div>
-                                        </div>`;
-            } else {
-                title.classList.add("selected");
-                const icon = document.createElement('i');
-                icon.classList.add('fa-solid', 'fa-star');
-                title.appendChild(icon);
-
-                const box = title.closest('.box');
-                const clonedBox = box.cloneNode(true);
-                clonedBox.classList.remove('active');
-
-                const selection = document.getElementById("select-container");
-                selection.innerHTML = '';
-                selection.appendChild(clonedBox);
             }
         });
-    }
 
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-        addBoxEventListener(box);
-        const title = box.querySelector('.title h6');
-        addTitleEventListener(title);
+        const isSelected = title.classList.contains("selected");
+
+        if (isSelected) {
+            title.classList.remove("selected");
+            const icon = title.querySelector('i');
+            if (icon) {
+                icon.remove();
+            }
+            const selection = document.getElementById("select-container");
+            selection.innerHTML = `<div class="box" id="choosed">
+                                        <img src="">
+                                        <div class="title">
+                                            <h6>Seleziona il tuo album cliccando sul nome</h6>
+                                        </div>
+                                    </div>`;
+        } else {
+            title.classList.add("selected");
+            const icon = document.createElement('i');
+            icon.classList.add('fa-solid', 'fa-star');
+            title.appendChild(icon);
+
+            const box = title.closest('.box');
+            const clonedBox = box.cloneNode(true);
+            clonedBox.classList.remove('active');
+            clonedBox.id = 'choosed';
+
+            const selection = document.getElementById("select-container");
+            selection.innerHTML = '';
+            selection.appendChild(clonedBox);
+        }
     });
+}
 
-    // Caricare il valore di val dal localStorage
-    var val = parseInt(localStorage.getItem('val'), 10);
-    if (isNaN(val) || val === 0) {
-        val = 0;
-    }
+function showSelectedAlbums() {
+    const container = document.querySelector('.container');
+    const checkboxes = document.querySelectorAll('#checkbox input[type="checkbox"]');
+    const selectedAlbums = [];
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            selectedAlbums.push(checkbox.nextElementSibling.textContent);
+        }
+    });
+    container.innerHTML = '';
+    selectedAlbums.forEach(album => {
+        const box = document.createElement('div');
+        box.classList.add('box');
 
-    var h3 = document.getElementById("point");
-    h3.textContent = val;
+        const img = document.createElement('img');
+        img.src = `img/${album.replace(/ /g, '_').replace(/>/g, '_').replace(/\?/g, 'xxx')}.jpg`;
 
-    function showSelectedAlbums() {
-        const checkboxes = document.querySelectorAll('#checkbox input[type="checkbox"]');
-        const selectedAlbums = [];
-        checkboxes.forEach((checkbox) => {
-            if (checkbox.checked) {
-                selectedAlbums.push(checkbox.nextElementSibling.textContent);
-            }
-        });
-        container.innerHTML = '';
-        selectedAlbums.forEach(album => {
-            const box = document.createElement('div');
-            box.classList.add('box');
+        const title = document.createElement('div');
+        title.classList.add('title');
 
-            const img = document.createElement('img');
-            img.src = `img/${album.replace(/ /g, '_').replace(/>/g, '_').replace(/\?/g, 'xxx')}.jpg`;
+        const h6 = document.createElement('h6');
+        if (album == "did you know that there's...") {
+            h6.classList.add("long");
+        }
+        h6.textContent = album;
 
-            const title = document.createElement('div');
-            title.classList.add('title');
+        title.appendChild(h6);
+        box.appendChild(img);
+        box.appendChild(title);
+        container.appendChild(box);
 
-            const h6 = document.createElement('h6');
-            if (album == "did you know that there's...") {
-                h6.classList.add("long");
-            }
-            h6.textContent = album;
+        addBoxEventListener(box);
+        addTitleEventListener(h6);
+    });
+    list();
+}
 
-            title.appendChild(h6);
-            box.appendChild(img);
-            box.appendChild(title);
-            container.appendChild(box);
-
-            addBoxEventListener(box);
-            addTitleEventListener(h6);
-        });
-        list();
-    }
-
-    function list() {
-        const list = document.querySelector(".list");
-        list.classList.toggle("hidden");
-    }
-
-    window.showSelectedAlbums = showSelectedAlbums;
-    window.list = list;
-    window.add = add;
-    window.remove = remove;
-    window.selectAll = selectAll;
-    window.deselectAll = deselectAll;
-
-    var h3 = document.getElementById("point");
-    h3.textContent = val;
-});
-
-var val = 0;
+function list() {
+    const list = document.querySelector(".list");
+    list.classList.toggle("hidden");
+}
 
 function add() {
     var h3 = document.getElementById("point");
-
+    var val = parseInt(localStorage.getItem('val'), 10) || 0;
     val++;
     h3.textContent = val;
     localStorage.setItem('val', val);
@@ -144,7 +115,7 @@ function add() {
 
 function remove() {
     var h3 = document.getElementById("point");
-
+    var val = parseInt(localStorage.getItem('val'), 10) || 0;
     val--;
     h3.textContent = val;
     localStorage.setItem('val', val);
@@ -152,8 +123,6 @@ function remove() {
 
 function selectAll() {
     const checkboxes = document.querySelectorAll('#checkbox input[type="checkbox"]');
-    const its = document.querySelectorAll('.it #checkbox input[type="checkbox"]');
-    const ens = document.querySelectorAll('.en #checkbox input[type="checkbox"]');
     const it = document.getElementById('it');
     const en = document.getElementById('en');
     
@@ -185,25 +154,7 @@ function deselectAll() {
     });
 }
 
-window.onload = function() {
-    showSelectedAlbums();
-    list();
-    var storedVal = parseInt(localStorage.getItem('val'), 10);
-    if (!isNaN(storedVal) && storedVal !== 0) {
-        val = storedVal;
-        var h3 = document.getElementById("point");
-        h3.textContent = val;
-    }
-    const selection = document.getElementById("select-container");
-    selection.innerHTML = `<div class="box">
-                                <img src="">
-                                <div class="title">
-                                    <h6>Seleziona il tuo album cliccando sul nome</h6>
-                                </div>
-                            </div>`;
-}
-
-function en(){
+function en() {
     const en = document.getElementById("en");
     var ens = document.querySelectorAll(".en");
 
@@ -213,7 +164,7 @@ function en(){
     });
 }
 
-function it(){
+function it() {
     const it = document.getElementById("it");
     var its = document.querySelectorAll(".it");
 
@@ -222,3 +173,70 @@ function it(){
         element.classList.toggle("hidden");
     });
 }
+
+function initialize() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        addBoxEventListener(box);
+        const title = box.querySelector('.title h6');
+        addTitleEventListener(title);
+    });
+
+    var val = parseInt(localStorage.getItem('val'), 10);
+    if (isNaN(val) || val === 0) {
+        val = 0;
+    }
+
+    var h3 = document.getElementById("point");
+    h3.textContent = val;
+
+    showSelectedAlbums();
+    list();
+
+    var storedVal = parseInt(localStorage.getItem('val'), 10);
+    if (!isNaN(storedVal) && storedVal !== 0) {
+        val = storedVal;
+        h3.textContent = val;
+    }
+
+    const selection = document.getElementById("select-container");
+    selection.innerHTML = `<div class="box" id="choosed">
+                                <img src="">
+                                <div class="title">
+                                    <h6>Seleziona il tuo album cliccando sul nome</h6>
+                                </div>
+                            </div>`;
+}
+
+function hide() {
+    const selectedAlbum = document.getElementById('choosed');
+    const dentro = document.querySelector('#choosed .title');
+    const selected = document.querySelectorAll('.box .title .selected')[1];
+    if (selectedAlbum) {
+        if (selectedAlbum.classList.contains('active')) {
+            selectedAlbum.classList.remove('active');
+            dentro.classList.remove('hidden');
+            if (selected) {
+                console.log(selected);
+                selected.classList.remove('not-selected');
+            }
+        } else {
+            selectedAlbum.classList.add('active');
+            dentro.classList.add('hidden');
+            if (selected) {
+                console.log(selected);
+                selected.classList.add('not-selected');
+            }
+        }
+    }
+}
+
+window.onload = initialize;
+window.showSelectedAlbums = showSelectedAlbums;
+window.list = list;
+window.add = add;
+window.remove = remove;
+window.selectAll = selectAll;
+window.deselectAll = deselectAll;
+window.en = en;
+window.it = it;
